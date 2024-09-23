1121725a182c3991952da2a77d559eae97fec7b5
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2024-09-23 19:24:43 +0530

    Add files via upload

---
+++ Array/JumpGame.java
@@ -0,0 +1,24 @@
+package com.tanmay.LeetCode150Medium.Array;
+
+public class JumpGame {
+
+	public static void main(String[] args) {
+		int[] nums = {1,1,2,3,5,2,0,0,0,1,3};
+		System.out.println(canJump(nums));
+	}
+	
+	public static boolean canJump(int[] nums) 
+	{
+		int finalIndex = nums.length - 1;
+		for(int i=nums.length-2;i>=0;i--)
+		{
+			if(i + nums[i] >= finalIndex)
+			{
+				finalIndex = i;
+			}			
+		}
+		
+		return finalIndex == 0;
+	}
+
+}
