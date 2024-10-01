ff2398fc57e9af22e7fddcc25673c4c7bd6da4da
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2024-10-01 18:15:50 +0530

    Add files via upload

---
+++ TwoPointer/ContainerWithMostWater.java
@@ -0,0 +1,31 @@
+package com.tanmay.TwoPointer;
+
+public class ContainerWithMostWater {
+
+	public static void main(String[] args) {
+		int[] heights = {1,8,6,2,5,4,8,3,8};
+		System.out.println(maxArea(heights));
+	}
+	
+	public static int maxArea(int[] heights)
+	{
+		int start = 0;
+		int end = heights.length-1;
+		int highestArea = 0;
+		
+		while(start < end)
+		{
+			highestArea = Math.max(highestArea, Math.min(heights[start], heights[end]) * (end - start));
+			if(heights[start] < heights[end])
+			{
+				start++;
+			}
+			else 
+			{
+				end--;
+			}
+		}
+		return highestArea;
+	}
+
+}
