35a90e0851cbcf5a101cb40f3a680b5f58f36f04
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2024-10-03 22:04:13 +0530

    Add files via upload

---
+++ String/ZigzagConversion.java
@@ -0,0 +1,41 @@
+package com.tanmay.String;
+
+public class ZigzagConversion {
+
+	public static void main(String[] args) {
+		String s = "PAYPALISHIRING";
+		int numRows = 3;
+		System.out.println(convert(s, numRows));
+	}
+	
+	public static String convert(String s, int numRows) {
+        String[] str = new String[numRows];
+        for(int i=0;i<numRows;i++)
+        {
+            str[i] = "";
+        }
+        int i = 0;
+        while(i<s.length())
+        {
+            for(int j=0;j<numRows && i<s.length();j++)
+            {
+                str[j] = str[j] + s.charAt(i++);
+                // i++;
+            }
+            for(int j=numRows-2;j>0 && i<s.length();j--)
+            {
+                str[j] = str[j] + s.charAt(i++);
+                // i++;
+            }
+        }
+
+        String ans = "";
+        for(String a : str)
+        {
+            ans = ans+a;
+        }
+
+        return ans;
+    }
+
+}
