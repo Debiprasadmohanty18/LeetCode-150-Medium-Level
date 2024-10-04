71c31450eb6cabed357ff73fd194ee0447d4afcd
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2024-10-04 21:03:13 +0530

    Add files via upload

---
+++ Array/H_Index.java
@@ -0,0 +1,33 @@
+package com.tanmay.LeetCode150Medium.Array;
+
+public class H_Index {
+
+	public static void main(String[] args) 
+	{
+		int[] citations = {3,0,6,1,5};
+		System.out.println(hIndex(citations));
+	}
+	
+	public static int hIndex(int[] citations) {
+        int[] author = new int[citations.length+1];
+        
+        for(int i=0;i<citations.length;i++)
+        {
+        	if(citations[i] < citations.length)
+        		author[i]++;
+        	else 
+        		author[citations.length]++;
+        }
+        
+        int count = 0;
+        for(int i=citations.length;i>=0;i--)
+        {
+        	count = count + author[i];
+        	if(count >= i)
+        	{
+        		return i;
+        	}
+        }
+        return 0;
+    }
+}
